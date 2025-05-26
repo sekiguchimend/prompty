7d95996a3cd3f39eb016bf983df5896bb6b43f65
sekiguchimend <sekiguchishunya0619@gmail.com>
2025-05-26 09:01:08 +0900

    型エラーの修正

--- components/prompt/prompt-card.tsx
+++ components/prompt/prompt-card.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { formatDate, truncateText } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import { formatDate, truncateText } from '../../lib/utils';
+import { Button } from '../../src/components/ui/button';
 import { UnifiedAvatar } from '../../src/components/index';
 
 interface PromptCardProps {
